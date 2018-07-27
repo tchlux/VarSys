aa962a9a66250c0aebf8197e54a75fe6fe7727c0
Thomas Lux <thomas.ch.lux@gmail.com>
2018-07-27 09:01:23 -0500

    Cleaned up files, removed auto-generated files, started performing analysis.

--- Code_[2018-06-11]_Stable_Arbitrary_Box_Splines/Testing_Fortran/analyze_results.py
+++ Code_[2018-06-11]_Stable_Arbitrary_Box_Splines/Testing_Fortran/analyze_results.py
@@ -10,7 +10,7 @@ if not os.path.exists(all_data_file):
     d = d1[:]
     d += d2
     d.save("performance_data.csv")
-    d.save("performance_data.pkl")
+    d.save(all_data_file)
 
 # Load data from file and reduce it to the unique tests that were run.
 d = Data.load(all_data_file)
@@ -18,19 +18,13 @@ tests = d.names[:-2]
 d = d[tests].unique().collect(d)
 d["Num Trials"] = map(len, d["Time"])
 d[d["Optimization"] == None, "Optimization"] = ""
+d["Computer"] = (c[0] for c in d["Computer"])
+d["Time"] = d.pop("Time")
 
 # Print out summary information about the data.
 d.summarize()
 print(d)
-
-d = d[d["Multiplicity"] <= 2]
-d = d[d["Version"] == "bs-manual.f90"]
-for c in sorted(set(d["Compiler"])):
-    print()
-    print(c)
-    print(d[d["Compiler"] == c, d.names[:-4]].unique()[::-1])
-
-exit()
+print()
 
 # Check for nonuniform numbers of executions for different settings.
 if len(set(d["Num Trials"])) > 1:
@@ -57,4 +51,64 @@ if len(set(d["Num Trials"])) > 1:
     exit()
 
 # 
-from util.plot import Plot
+from util.stats import cdf_fit_func
+from util.plot import Plot, multiplot
+import numpy as np
+
+legend_settings = dict(
+    xanchor = "center",
+    yanchor = "top",
+    x = .5,
+    y = -.18,
+    orientation = "h",
+)
+
+# Break up the data by compiler
+ifort = d[d["Compiler"] == "ifort"]
+gfort = d[d["Compiler"] == "gfortran"]
+sun95 = d[d["Compiler"] == "/home/f/ltw/bin/ftn95.sun"]
+# Set the compiler names
+ifort.compiler = "ifort"
+gfort.compiler = "gfortran"
+sun95.compiler = "sun95"
+
+# Order the different comparitors by which is best.
+compilers = [gfort, ifort, sun95]
+optimizations = sorted(set(d["Optimization"]))
+versions = ["bs-dynamic.f90", "bs-automatic.f90", "bs-manual.f90"]
+
+
+# Plot by optimization level for each compiler.
+plots = []
+for c in compilers:
+    p = Plot(f"Aggregate Runtime CDFs for Different Optimizations on Each Compiler for {len(c)//4} Tests", 
+             "Execution Time (sec)", c.compiler)
+    for i,opt in enumerate(optimizations):
+        perf_data = [v for t in c[c["Optimization"]==opt, "Time"] for v in t[0]]
+        cdf = cdf_fit_func(perf_data)
+        if (opt.strip() == ""): opt = "No Optimization"
+        p.add_function(opt, cdf, cdf(), group=opt, show_in_legend=(c==compilers[0]))
+    plots.append(p.plot(html=False, y_range=[.5,1], 
+                        legend=legend_settings))
+multiplot([[p] for p in plots], show=False, append=True, shared_x=True)
+
+# 
+plots = []
+for c in compilers:
+    name = c.compiler
+    # Plot by code version for Sun.
+    c = c[ c["Optimization"] == "-O3" ]
+    c = c[ c["Num Points"] == "4K" ]
+    first = (name==compilers[0].compiler)
+    p = Plot(f"'O3' Runtime CDFs for Different Box-Spline Versions on Each Compiler<br>over {len(c)//3} Tests, Each with 4K Evaluation Points", 
+             "Execution Time (sec)", f"{name}")
+    for i,version in enumerate(versions):
+        perf_data = [v for t in c[c["Version"]==version, "Time"] for v in t[0]]
+        cdf = cdf_fit_func(perf_data)
+        if ("manual" in version): version = version.replace("manual", "allocate")
+        version = version.replace("bs-","").replace(".f90","")
+        p.add_function(version, cdf, cdf(), color=p.color(i), group=version,
+                       show_in_legend=first)
+    plots.append( p.plot(html=False, legend=legend_settings, y_range=[.4,1]) )
+multiplot([[p] for p in plots], append=True, shared_x=True)
+
