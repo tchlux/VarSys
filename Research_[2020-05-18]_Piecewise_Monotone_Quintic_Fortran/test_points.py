Fixed a numerical error causing local quadratics to have zero curvature. Finished implementing test code to verify future improvements. Ready to implement next steps of code.

Research_[2020-05-18]_Piecewise_Monotone_Quintic_Fortran/test_points.py
@@ -3,15 +3,24 @@ TESTS = {}
 
 # --------------------------------------------------------------------
 # Watson test.
-import numpy as np
-from polynomial import Spline, fit
-y = np.array([0,1,1,1,0,20,19,18,17,0,0,3,0,1,6,16,16.1,1], dtype=float)
-x = np.linspace(0,1,len(y))
-TESTS["watson_test"] = Spline(x, y[:,None])
+from polynomial import Spline
+y = [[0,1],[1,0],[1,0],[1,0],[0,0], # Plateu with curvature only on ends.
+     [20,-1],[19,-1],[18,-1],[17,-1], # Sudden linear segment
+     [0,0],[0,0],[3,0],[0,0], # Sudden flat with one peak
+     [1,3],[6,9], # Exponential growth
+     [16,.1],[16.1,.1], # small linear growth
+     [1,-15]]
+x = list(range(len(y)))
+# Convert to cover the unit interval.
+x = [v/(len(y)-1) for v in x]
+for i in range(len(y)): y[i][1] *= (len(y)-1)
+# Store the test.
+TESTS["piecewise_polynomial"] = Spline(x, y)
 
 # --------------------------------------------------------------------
-# Sin function.
-TESTS["sin_function"] = np.sin
+# Signal function.
+from numpy import sin, pi
+TESTS["signal"] = lambda x: sin(4 * (2*pi) * x) / (x**2 + .1)
 
 # --------------------------------------------------------------------
 # Large tangent test.
@@ -19,6 +28,7 @@ TESTS["large_tangent"] = lambda x: -(1.0 + ( 1.0 / (x-0.99) ))
 
 # --------------------------------------------------------------------
 # Random monotone data test.
-TESTS["random_monotone"] = lambda x: np.random.random(size=len(x))
+from numpy import random
+TESTS["random"] = lambda x: random.random(size=len(x)) if hasattr(x,"__len__") else random.random()
 
 

Research_[2020-05-18]_Piecewise_Monotone_Quintic_Fortran/test_splines.py
@@ -201,19 +201,22 @@ if TEST_PMQSI:
     from util.system import Timer
     import numpy as np
     VISUALIZE_TEST = True
+    RANDOM = False
+    NUM_KNOTS = 18
+    FUNC = TESTS[sorted(TESTS)[-1]]
 
-    # np.random.seed(2)
-    # # Generate some random knots and values.
-    # num_knots = 10
-    # knots = 10 * np.random.random(size=(num_knots))
-    # knots.sort()
-    # values = 10 * (2*(np.random.random(size=(num_knots,)) - 1/2))
+    # Generate some random knots and values.
+    if RANDOM:
+        np.random.seed(0)
+        knots = np.random.random(size=(NUM_KNOTS))
+        knots.sort()
+    else:
+        knots = np.linspace(0,1,NUM_KNOTS)
 
     # Get the knots and values for the test.
-    knots, values = TESTS[sorted(TESTS)[-3]]
+    values = list(map(FUNC, knots))
     knots = np.asfortranarray(knots, dtype=float)
     values = np.asfortranarray(values, dtype=float)
-    num_knots = len(knots)
 
     print()
     print(f"knots ({len(knots)}):\n  {knots}")
@@ -242,8 +245,11 @@ if TEST_PMQSI:
     truth = monotone_quintic_spline(knots, values, exact=True)
     t.stop() ; print(f"Python time: {t()}s")
     # --------------------------------------------------------
+    # for v in truth.values:
+    #     print("", [round(float(_),2) for _ in v])
 
-    if VISUALIZE_TEST and (num_knots <= 50):
+    if VISUALIZE_TEST and (len(knots) <= 100):
+        print()
         from util.plot import Plot
         # Make a pretty picture of the B-spline and its derivatives.
         padding = 0 #.1
@@ -252,7 +258,6 @@ if TEST_PMQSI:
             x += list(np.linspace(knots[i], knots[i+1], 10))[1:]
         x = np.array(x, dtype=float)
         y, info = splines.eval_spline(sk, sc, x.copy(), d=0)
-        print("y: ",y)
         p = Plot("Polynomial Interpolant")
         p.add("Spline", x, y, mode="lines", color=p.color(1))
         true_y = list(map(truth, x))